ca233f74179f8a03996e9d250f0783ae43c087f0
Eddie Madrigal <edmadrigal@yahoo.com>
2020-07-21 12:17:08 -0700

    iterative sorting function refactored

--- quicksort.py
+++ quicksort.py
@@ -15,15 +15,15 @@ def quick_sort(sequence):
         print('pivot: ' + str(pivot))
         if item > pivot: # if the item is greater than the pivot point
             items_greater.append(item) # append the item to the list that is greater than
-            print('items greater than pivot: ' + str(items_greater))
         else:
             items_lower.append(item) # also includes the items that are equal
-            print('items less than pivot: ' + str(items_lower))
 
     # now, what we need to do is apply this algorithm over and over to each of the sub lists that we create
     # we'll do this with a return statement.  ie, a return should apply this algorithm again to each of the
     # of the items lower, we should have that pivot point in the center and we apply the algorithm again
     # to the items greater. the length of sequence shortens every time; this process repeats until sequence is >= 1
+    # the return statement concatenates lists (pivot is put into a list for concat purpuses)
+    print(str(items_lower) + ' | ' + str(pivot) + ' | ' + str(items_greater))
     return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
 
 arr = [0, 9, 3, 8, 2, 7, 5]
